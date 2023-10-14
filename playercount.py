345f9b6ce5479eff6894592c12066a259a078a2a
Matthew Yau <myau0005@student.monash.edu>
2023-10-15 09:43:42 +1100

    a

--- playercount.py
+++ playercount.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv("C:\\Users\\thema\\OneDrive\\Desktop\\Folder\\3179\\data\\Unique Athletes.csv")
+data = pd.read_csv("C:\\Users\\thema\\OneDrive\\Desktop\\Folder\\3179\\data\\athlete_events.csv")
 
 df = pd.DataFrame(data)
 
@@ -8,6 +8,6 @@ df = df[~df['Season'].str.contains('Winter')]
 
 df = df.dropna(subset = ["Medal"])
 
-medals = df.groupby(['Year', 'NOC'])['Medal'].count()
+medals = df.groupby(['Year', 'Team'])['Medal'].count()
 
-medals.to_csv("Medal Count Each Year.csv", index=True)
+medals.to_csv("Medal Count Each Year c.csv", index=True)
