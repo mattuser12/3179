b3b3b48a9ef1c35628e45ccd0de72843f33cb078
Matthew Yau <myau0005@student.monash.edu>
2023-10-15 17:17:15 +1100

    a

--- summerwinter.py
+++ summerwinter.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("C:\\Users\\thema\\OneDrive\\Desktop\\Folder\\3179\\data\\athlete_events.csv")
 
-players_count = df.groupby(['Year', 'Season', 'Sex', 'City']).size().reset_index(name='Player_Count')
+players_count = df.groupby(['Year', 'Season', 'Sex', 'City', 'Sport']).size().reset_index(name='Player_Count')
 
 winter_olympics = players_count[players_count['Season'] == 'Winter']
 summer_olympics = players_count[players_count['Season'] == 'Summer']
 
-winter_olympics.to_csv("Winter Olympics.csv", index = True)
-summer_olympics.to_csv("Summer Olympics.csv", index = True)
+winter_olympics.to_csv("Winter Olympics sport.csv", index = True)
+summer_olympics.to_csv("Summer Olympics sport.csv", index = True)
