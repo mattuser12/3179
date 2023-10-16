54eedbb7fa3f8ca65673e4c952a704b928b7384c
Matthew Yau <myau0005@student.monash.edu>
2023-10-16 11:05:16 +1100

    a

---
+++ gender.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv("C:\\Users\\thema\\OneDrive\\Desktop\\Folder\\3179\\data\\Unique Athletes.csv")
+
+df = df.groupby(['Year', 'Season', 'Sex']).size().reset_index(name='Count')
+
+df = df.drop(df.index[0])
+
+df_pivot = df.pivot_table(index=['Year', 'Season'], columns='Sex', values='Count').reset_index()
+
+df_pivot['Male Ratio'] = df_pivot['M'] / (df_pivot['M'] + df_pivot['F'])
+df_pivot['Female Ratio'] = df_pivot['F'] / (df_pivot['M'] + df_pivot['F'])
+
+summer_df = df_pivot[df_pivot['Season'] == 'Summer']
+winter_df = df_pivot[df_pivot['Season'] == 'Winter']
+
+summer_df.to_csv("Summer Ratio.csv", index = True)
+winter_df.to_csv("Winter Ratio.csv", index = True)
--- summerwinter.py
+++ summerwinter.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("C:\\Users\\thema\\OneDrive\\Desktop\\Folder\\3179\\data\\athlete_events.csv")
 
-players_count = df.groupby(['Year', 'Season', 'City', 'Sport']).size().reset_index(name='Player_Count')
+players_count = df.groupby(['Season', 'Sport']).size().reset_index(name='Player_Count')
 
 winter_olympics = players_count[players_count['Season'] == 'Winter']
 summer_olympics = players_count[players_count['Season'] == 'Summer']
 
-winter_olympics.to_csv("Winter Olympics sport.csv", index = True)
-summer_olympics.to_csv("Summer Olympics sport.csv", index = True)
+print(winter_olympics.nlargest(10, 'Player_Count')[['Sport', 'Player_Count']])
+
+# winter_olympics.to_csv("Winter Olympics sport.csv", index = True)
+# summer_olympics.to_csv("Summer Olympics sport.csv", index = True)
